b40a7f835c2b6b3401c35c33d00e78301ea1aecf
miqqo <mikko291@student.liu.se>
2015-03-05 16:00:12 +0100

    added drop down button

--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,8 @@
 import 'package:angular/application_factory.dart';
 import 'package:di/annotations.dart';
+import 'package:bootjack/bootjack.dart';
 
+//import 'menu.dart';
 
 @Injectable()
 class Greeter {
@@ -11,4 +13,8 @@ void main() {
   applicationFactory()
       .rootContextType(Greeter)
       .run();
+  
+  //dropdown
+   Dropdown.use();
+  
 }
