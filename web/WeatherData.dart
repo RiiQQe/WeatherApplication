a70f0783336c1121228377f92a3a9fd7076e5451
petraohlin8 <petraohlin8@gmail.com>
2015-03-06 15:30:16 +0100

    added WeatherData class that finally works

---
+++ web/WeatherData.dart
@@ -0,0 +1,50 @@
+import 'package:angular/application_factory.dart';
+import 'package:di/annotations.dart';
+import 'dart:html';
+import 'dart:convert';
+import 'dart:async';
+
+class WeatherData{
+  
+  Map allData;
+  double latitude, longitude;
+  
+  WeatherData(this.latitude, this.longitude);
+  
+  loadData() {
+    print("Loading data");
+    
+    //create URL to SMHI-API with longitude and latitude values
+    var url = 'http://opendata-download-metfcst.smhi.se/api/category/pmp1.5g/version/1/geopoint/lat/$latitude/lon/$longitude/data.json';
+
+    //call SMHI-API and catch error if any
+    //pass request content to onDataLoaded
+    HttpRequest.getString(url).then((String responseText){
+     
+     //parse response text
+     allData = JSON.decode(responseText); 
+     
+     //debug check if the right data is printed
+     print(allData["referenceTime"]); 
+     print(allData["timeseries"][0]["ws"]); 
+     
+     querySelector("#start-lat").text = "${allData["referenceTime"]}";
+     printData();
+
+
+  }, onError: (error) => printError(error));
+
+  }
+  
+  void printError(error){
+    print("It doesn't work, too bad! Error code: ${error.code}");
+  }
+  
+  
+  printData(){
+    print(allData["referenceTime"]);
+  }
+  
+}
+
+
--- web/main.dart
+++ web/main.dart
@@ -2,7 +2,9 @@ import 'package:angular/application_factory.dart';
 import 'package:di/annotations.dart';
 import 'dart:html';
 import 'dart:convert';
+import 'dart:async';
 
+import 'WeatherData.dart';
 
 @Injectable()
 class Greeter {
@@ -19,8 +21,9 @@ main() {
   //Uncomment when compilated to JavaScript
   //coordinates  = findCoords();
   
-  loadData(coordinates[0], coordinates[1]);
-
+  WeatherData currentWeather = new WeatherData(coordinates[0], coordinates[1]);
+  currentWeather.loadData();
+  
 }
 
 void printError(error){
@@ -31,38 +34,14 @@ void printError(error){
 findCoords(){
   window.navigator.geolocation.getCurrentPosition().then((Geoposition pos){
 
-    double lat = pos.coords.latitude;                    //Here the variables are set
-    double long = pos.coords.longitude;
-    
-    querySelector("#start-lat").text = "${pos.coords.latitude}";
-    querySelector("#start-long").text = "${pos.coords.longitude}";
-    print('hejhej');
-    var coordinates = [lat, long];
-    return coordinates;
+  double lat = pos.coords.latitude;                    //Here the variables are set
+  double long = pos.coords.longitude;
+   
+  var coordinates = [lat, long];
+  return coordinates;
     
   }, onError: (error) => printError(error));
  
 }
 
-loadData(double lat, double lon) {
-  print("Loading data");
-  
-  //create URL to SMHI-API with longitude and latitude values
-  var url = 'http://opendata-download-metfcst.smhi.se/api/category/pmp1.5g/version/1/geopoint/lat/$lat/lon/$lon/data.json';
-
-  //call SMHI-API and catch error if any
-  //pass request content to onDataLoaded
-  var request = HttpRequest.getString(url).then(onDataLoaded); //catchError() missing
-}
-
-onDataLoaded(String responseText) {
-   
-   //parse response text
-   Map data = JSON.decode(responseText); 
-   
-   //debug check if the right data is printed
-   print(data["timeseries"][0]["pis"]); 
-   print(data["timeseries"][0]["ws"]); 
-   
-}
 
