dcbf00bead2faeacf9436d488b8b3514a49a5f24
RiiQQe <rickard.lindstedt@gmail.com>
2015-03-06 10:26:54 +0100

    Added coordinates

--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,10 @@
 import 'package:angular/application_factory.dart';
 import 'package:di/annotations.dart';
+import 'dart:html';
 
+
+//Coordinates
+var long, lat;
 
 @Injectable()
 class Greeter {
@@ -8,7 +12,27 @@ class Greeter {
 }
 
 void main() {
+ 
+  findCoords();
+  
   applicationFactory()
       .rootContextType(Greeter)
       .run();
+}
+
+void alertError(PositionError error) {
+  window.alert("Error occurred. Error code: ${error.code}");
+}
+
+//Function to set coordinates
+void findCoords(){
+  window.navigator.geolocation.getCurrentPosition().then((Geoposition pos){
+
+    lat = pos.coords.latitude;                    //Here the variables are set
+    long = pos.coords.longitude;
+    
+    querySelector("#start-lat").text = "${pos.coords.latitude}";
+    querySelector("#start-long").text = "${pos.coords.longitude}";
+  }, onError: (error) => alertError(error));
+ 
 }
