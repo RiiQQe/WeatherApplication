5e066dca6c811773ab6e9f7f37c2cc08750f58f1
petraohlin8 <petraohlin8@gmail.com>
2015-03-20 10:01:49 +0100

    Added filter formatter

---
+++ lib/formatter/time_category_filter.dart
@@ -0,0 +1,18 @@
+library time_category_filter;
+
+import 'package:angular/angular.dart';
+
+
+@Formatter(name: 'categoryfilter')
+class CategoryFilter {
+  List call(weatherSets, categoryFilterMap) {
+    if (weatherSets is Iterable && categoryFilterMap is Map) {
+      // If there is nothing checked, treat it as "everything is checked"
+      bool nothingChecked = categoryFilterMap.values.every((isChecked) => !isChecked);
+      return nothingChecked
+          ? weatherSets.toList()
+          : weatherSets.where((i) => categoryFilterMap[i.category] == true).toList();
+    }
+    return const [];
+  }
+}
