dd0f9bd8aa9d21ffe4dcfc37957204e5b02e8388
RiiQQe <rickard.lindstedt@gmail.com>
2015-05-29 14:20:27 +0200

    Now it YR_load works properly

--- lib/component/load_yr.dart
+++ lib/component/load_yr.dart
@@ -10,9 +10,6 @@ import 'dart:html';
 import 'package:intl/intl.dart';
 import 'dart:async';
 
-import 'package:xml/xml.dart';
-
-
 import 'package:weatherapplication/component/weather_data.dart';
 
 class LoadYr {
@@ -30,7 +27,7 @@ class LoadYr {
     
     //Url to proxy server that calls yr API with passed parameters
     var url = 'http://xn--petrahlin-47a.se/proxy.php?lon=$longitudeString&lat=$latitudeString';
-    url = 'http://xn--petrahlin-47a.se/proxy.php?lon=9.58&lat=60.10';
+    //url = 'http://xn--petrahlin-47a.se/proxy.php?lon=9.58&lat=60.10';
     
 
     
@@ -51,169 +48,73 @@ class LoadYr {
 
 
   void setWeatherParameters() {
-    String cloud, rain, wind, timeFormatted, currentWeather;
-    double currentTemp, currentWind, currentCloud, currentRain;
-    DateTime currentTime;
+    var cloud, rain, wind, timeFormatted, currentWeather;
+    var currentTemp, currentWind, currentCloud, currentRain;
+    DateTime currentTimeTemp, currentTimeRain;
     bool readRain = false;
+    var currentSymbol;
     
     weatherSets.clear();
    
-    //List<XmlElement> temperatures = allData.findAllElements('temperature');
-    //List<XmlNode> times = allData.findAllElements('time');
-    //List<XmlNode> winds = allData.findAllElements('windSpeed');
-    //List<XmlNode> clouds = allData.findAllElements('cloudiness');
-    //List<XmlNode> rains = allData.findAllElements('precipitation');
-    //List<XmlNode> weather = allData.findAllElements('symbol');
   
     List<XmlElement> times = allData.findAllElements("time");
     
-    /*while( i < times.length ){
-      var date = times.elementAt(i).attributes.elementAt(1).value;
-        
-      print("date: " + date.toString());
-      
-      var location = times.elementAt(i).children;
-        
-      print("location: " + location.toString());
-      
-      
-      
-      print("===================================");
-      i++;
-    }*/
+    var tempDate, rainDate;
+    var boothDone = false;
+    //var currentTemp, currentWind, currentCloud, currentRain;
     
     
     times.forEach((time){
-      print("=================== new date ==========================");
       if(time.findAllElements("temperature").length != 0){
-        var date = time.attributes.elementAt(1).value; // This date will be fucked
-        print("date: " + date.toString());
-        var currentTemp = time.findAllElements("temperature").elementAt(0).attributes.elementAt(2).value;
-        print("temp: " + currentTemp.toString());
+        tempDate = time.attributes.elementAt(1).value; 
+        currentTimeTemp = DateTime.parse(tempDate);
+        //print("date: " + date.toString());
+        currentTemp = double.parse(time.findAllElements("temperature").elementAt(0).attributes.elementAt(2).value);
+        currentWind = double.parse(time.findAllElements("windSpeed").elementAt(0).attributes.elementAt(1).value);
+        currentCloud = double.parse(time.findAllElements("cloudiness").elementAt(0).attributes.elementAt(1).value);
+
+        wind = getWind(currentWind);
+        cloud = getCloud(currentCloud);
+        rain = getRain(currentRain, currentWeather);
+              
+        timeFormatted = formatter.format(currentTimeTemp);
         
-        var currentWind = time.findAllElements("windSpeed").elementAt(0).attributes.elementAt(1).value;
-        
-        var currentCloud = time.findAllElements("cloudiness").elementAt(0).attributes.elementAt(1).value;
         readRain = true;
+    
       }else{
         if(readRain){
-          var date = time.attributes.elementAt(1).value; // This date will be fucked
-          var currentRain = time.findAllElements("precipitation").elementAt(0).attributes.elementAt(1).value;
+          //rainDate = time.attributes.elementAt(2).value; 
+          //currentTimeRain = DateTime.parse(rainDate);
+          currentRain = double.parse(time.findAllElements("precipitation").elementAt(0).attributes.elementAt(1).value);
+          currentSymbol = time.findAllElements("symbol").elementAt(0).attributes.elementAt(0).value;
           
-          print("rain: " + currentRain.toString());
-          
+
+          rain = getRain(currentRain, currentSymbol);
+
           readRain = false;
+          boothDone = true;
         }
       }
-      
-      
-     // print("date: " + time.toString());
-     // print("length: " + time.children.length.toString());
-      
-      
-     // print("child: " + children.toString());
-      
-    //  print("child length: " + children.length.toString());
-      
-      /*if(temp.toList().length != 0){
-        window.alert("temp is");
-        var tempString = temp.elementAt(0).attributes.elementAt(2);
-        print("temp " + tempString.toString());
-      }else{
-        
-      }*/
-    });
+        if(boothDone){
+          //To print and see that both works properly
+          /*print("====NEW DATE======");
+          print("tempDate: " + tempDate.toString());
+          print("rainDate: " + rainDate.toString());
+          print("======VALUES======");
+          print("temp: " + currentTemp.toString());
+          print("wind: " + currentWind.toString());
+          print("cloud: " + currentCloud.toString());
+          print("rain: " + currentRain.toString());*/
+          
+          weatherSets.add(new WeatherSet(currentTemp, cloud, rain, wind, timeFormatted, currentRain, currentWind, currentCloud, currentTimeTemp, currentTemp));
 
-    //print("temp.length = " + temperatures.length.toString());
-    //print("times.length = " + times.length.toString());
-    //print("winds.length = " + winds.length.toString());
-    //print("clouds.length = " + clouds.length.toString());
-    //print("rains.length = " + rains.length.toString());
-
-
-    
-
-
-   /*
-
-    for(int i = 0; i < temperatures.length; i++){
-        temps.add(double.parse(temperatures.elementAt(i).attributes.elementAt(2).value));
-        windsR.add(double.parse(winds.elementAt(i).attributes.elementAt(1).value));
-        cloudsR.add(double.parse(clouds.elementAt(i).attributes.elementAt(1).value));
-    }
-    int i = 0, rainCounter = 0;
-    print("rain");
-    while(i < rains.length){
-      rainsR.add(double.parse(rains.elementAt(i).attributes.elementAt(1).value)); 
-      weatherSymbols.add(weather.elementAt(i).attributes.elementAt(0).value); 
-
-      rainCounter++;
-      if(i < 2) i = i + 3;
-      else if(i < 250) i = i + 4;
-      else i++;
-    }
-    print("dates");
-    for(int i = 0; i < 60; i++){
-      dates.add(DateTime.parse(times.elementAt(i*5).attributes.elementAt(2).value));
-    }
-    print("after dates");
-    i = 0;
-    while(i < rainsR.length && i < temps.length && i < dates.length){
-      print(" ");
-      print("==========================");
-
-      print("time: " +dates[i].toString());
-      print("temp: " +temps[i].toString());
-      print("cloud: " +cloudsR[i].toString());
-      print("wind: " +windsR[i].toString());
-      print("rain: " +rainsR[i].toString());
-      i++;
-    }
-
-    print("lengths after: ");
-    print("temps  " + temps.length.toString());
-    print("rainsR " + rainsR.length.toString());
-
-*/
-/*
-    int timeCounter = 0;
-    int rainCounter = 0;
-    for(int i=0; i < temperatures.length ;i++){
-      if(i < 58){
-          timeCounter = i * 5;
-          rainCounter = i * 4;
-      }else{
-          timeCounter++;
-          rainCounter++;
-      }
-
-      currentTime = DateTime.parse(times.elementAt(timeCounter).attributes.elementAt(2).value);
-      currentRain = double.parse(rains.elementAt(rainCounter).attributes.elementAt(1).value);
-      currentWeather = weather.elementAt(rainCounter).attributes.elementAt(0).value;
-
-      currentTemp = double.parse(temperatures.elementAt(i).attributes.elementAt(2).value);
-      currentWind = double.parse(winds.elementAt(i).attributes.elementAt(1).value);
-      currentCloud = double.parse(clouds.elementAt(i).attributes.elementAt(1).value);
-        
-      print("===============================");
-      print("currentTime: " + currentTime.toString());
-
-      print("currentRain: " + currentRain.toString());
-      print("currentTemp: " + currentTemp.toString());
-      print("currentWind: " + currentWind.toString());
-      print("currentCloud: " + currentCloud.toString());
-
-      
-      
-      wind = getWind(currentWind);
-      cloud = getCloud(currentCloud);
-      rain = getRain(currentRain, currentWeather);
+          boothDone = false;
             
-      timeFormatted = formatter.format(currentTime);      
+            
+        }
+    });
+    print("loading YR done2!");
       
-      weatherSets.add(new WeatherSet(currentTemp, cloud, rain, wind, timeFormatted, currentRain, currentWind, currentCloud, currentTime, currentTemp));
-
-    }*/
   }
   
   //Primitive way of translating parameters from numbers to Strings
